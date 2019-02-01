2e45f13dda4b2499528e777d2fea1b21d37305c7
Roman <iboutlet@icloud.com>
2019-02-01 05:03:56 +0300

    aa
    now tap buttons delete when touched and don't consume memmory.

--- spiid/ViewController.swift
+++ spiid/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
     }
     
     @objc func playButtonAction(sender: UIButton!) {
-        sender.isHidden = true
+        sender.removeFromSuperview()
         spawnPlayButton()
     }
 }
