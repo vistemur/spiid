deebb72b3741e7add7d35db322509c7fcfc1e4fb
Roman <iboutlet@icloud.com>
2019-02-19 03:44:01 +0300

    score, timer, end
    score, timer, end

--- spiid/ViewController.swift
+++ spiid/ViewController.swift
@@ -12,27 +12,78 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    @IBAction func startButtunPressed(_ sender: UIButton) {
-        sender.isHidden = true
-        spawnPlayButton()
     }
     
+    @IBOutlet weak var startPlayButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    public var score = 0
+    public var game = false
+    
+    // TODO: startButtonPressed
+    @IBAction func startButtunPressed(_ sender: UIButton) {
+        score = 0
+        scoreLabel.isHidden = false
+        sender.isHidden = true
+        spawnPlayButton()
+        game = true
+        
+        // timer
+        let time = 60
+        var timeCount = 0
+        var timeTillTheEnd = time
+        
+        timeLabel.text = "\(timeTillTheEnd)"
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            timeCount += 1
+            timeTillTheEnd -= 1
+            self.timeLabel.text = "\(timeTillTheEnd)"
+            
+            ///timers end
+            if timeCount == time {
+                timer.invalidate()
+                self.timeIsOut()
+            }
+        }
+    }
+    
+    // ToDo: time is out
+    func timeIsOut() {
+        print("time is out")
+        timeLabel.text = "your score \(score)"
+        scoreLabel.isHidden = true
+        startPlayButton.isHidden = false
+        game = false
+    }
+    
+    // TODO: spawn button (for game)
     func spawnPlayButton() {
-        let playButton = UIButton(frame: CGRect(x: Int.random(in: 1...275),
-                                                y: Int.random(in: 1...525),
+        
+        let height = self.view!.frame.height
+        let width = self.view!.frame.width
+        
+        let playButton = UIButton(frame: CGRect(x: Int.random(in: 1...(Int(width) - 50)),
+                                                y: Int.random(in: 1...(Int(height) - 50)),
                                                 width: 50,
                                                 height: 50))
         playButton.setTitle("tap", for: .normal)
-         playButton.backgroundColor = UIColor.red
+        playButton.backgroundColor = UIColor.red
         playButton.addTarget(self, action: #selector(playButtonAction), for: .touchUpInside)
         self.view.addSubview(playButton)
     }
     
+    // playButton is touched
     @objc func playButtonAction(sender: UIButton!) {
-        sender.removeFromSuperview()
-        spawnPlayButton()
+        if game {
+            sender.removeFromSuperview()
+            spawnPlayButton()
+            score += 1
+            scoreLabel.text = "\(score)"
+        } else {
+            sender.removeFromSuperview()
+            game = false
+        }
     }
 }
